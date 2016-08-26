c4b7cc374f0660ce164839471b69dfc60896b0e3
Tomoyuki Kashiro <ksrtmyk@gmail.com>
2016-08-26 12:36:57 -0400

    feat(build): use CachedStaticFilesStorage for file storage (#2)

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 if DEBUG:
